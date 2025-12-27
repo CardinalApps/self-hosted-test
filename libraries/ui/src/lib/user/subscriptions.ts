070f645fb8033bbafdcfccdb3b7675c8c6390ed6
briiian <brian@cardinalapps.io>
2025-12-27 02:42:04 -0500

    chore: restore the deprecated code that was actually still needed
    Signed-off-by: briiian <brian@cardinalapps.io>
    
    internal-repo-sha: 6310636689eaf9d7c19f6721a8590c696fa3d792

---
+++ libraries/ui/src/lib/user/subscriptions.ts
@@ -0,0 +1,13 @@
+/**
+ * Keep the backend copy of this definition in sync. It is at:
+ *
+ * /server-auth/src/constants/subscriptions.js
+ * 
+ * @deprecated use @cardinalapps/products package instead
+ */
+const SUBSCRIPTIONS = {
+  FREE: 'free',
+  EARLY_ADOPTER: 'early_adopter',
+}
+
+export default Object.freeze(SUBSCRIPTIONS)
