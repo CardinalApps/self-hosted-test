bfe2e3f6f89c16b2f51b1c4452509a107f3812c7
briiian <brian@cardinalapps.io>
2026-04-12 17:29:03 -0400

    refactor: remove old page title bar
    Signed-off-by: briiian <brian@cardinalapps.io>
    
    internal-repo-sha: e5f499430ebd7e237aa4bbe78e2b267cab9c02f1

--- libraries/ui/src/components/features/AppBase/AppScaffold.tsx
+++ libraries/ui/src/components/features/AppBase/AppScaffold.tsx
@@ -2,12 +2,9 @@ import { useContext, useRef } from 'react'
 import type { PropsWithChildren, ReactNode } from 'react'
 import clsx from 'clsx'
 
-import Columns, { Column } from '../../layout/Columns'
-import DocLink from '../../interaction/DocLink'
 import SidebarNav from '../../interaction/SidebarNav'
 import { MiniAudioPlayer } from '../AudioPlayer'
 import { DirectoryTreeSidebarPortal, DirectoryTreeMobileButton } from '../../interaction/DirectoryTree'
-import H2 from '../../typography/H2'
 
 import { layoutSelectors } from '../../../store/slices/layout'
 import { PAGE_LAYOUT } from '../../../store/slices/layout'
@@ -37,24 +34,7 @@ function AppScaffold({
   const pageScrollRef = useRef(null)
   const mobileNavIsOpen = useAppSelector(layoutSelectors.mobileNavIsOpen)
   const layout = useAppSelector(layoutSelectors.current)
-  const pageTitle = useAppSelector(layoutSelectors.pageTitle)
-  const pageDocLink = useAppSelector(layoutSelectors.pageDocLink)
   const mobileFileBrowserIsOpen = useAppSelector(layoutSelectors.mobileFileBrowserIsOpen)
-
-  const hasPageTitleBar = () => {
-    const supportedLayouts = [
-      PAGE_LAYOUT.standard,
-      PAGE_LAYOUT.thin,
-      PAGE_LAYOUT.fixed,
-      PAGE_LAYOUT.files,
-    ]
-    if (supportedLayouts.includes(layout)) {
-      if (pageTitle || pageDocLink) {
-        return true
-      }
-    }
-    return false
-  }
 
   return (
     <div className={clsx('scaffold')} data-layout={layout}>
@@ -74,18 +54,6 @@ function AppScaffold({
       >
         <main className={clsx('page-content', !!mobileFileBrowserIsOpen && 'no-scroll')}>
           {layout === PAGE_LAYOUT.files && <DirectoryTreeMobileButton />}
-          {hasPageTitleBar() && (
-            <section className={clsx('page-title-bar')}>
-              <Columns>
-                <Column>
-                  {!!pageTitle && <H2 className="page-title">{pageTitle}</H2>}
-                </Column>
-                <Column>
-                  {!!pageDocLink && <DocLink href={pageDocLink} />}
-                </Column>
-              </Columns>
-            </section>
-          )}
           <Routes>
             {privateScaffoldRoutes ? privateScaffoldRoutes : null}
             {/* Top-level page error handling */}
